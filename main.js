ec911854a0621d57556ec157b4829c7fac4bfc16
Rexx Alfaro <rexxalfaro+github@gmail.com>
2024-03-30 22:46:57 -0700

    Added zoom in/out and text banner

---
+++ main.js
@@ -0,0 +1,20 @@
+var modal = document.getElementById('myModal');
+
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById('img1');
+var captionText = document.getElementById('caption');
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+modal.onclick = function(){
+    modalImg.className +=" out";
+    setTimeout(function(){
+        modal.style.display = "none";
+        modalImg.className = "modal-content";
+    },300);
+}
+
