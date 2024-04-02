a8189413b68950e16dfe24ab14a3ab0d4e632a7c
Rexx Alfaro <rexxalfaro+github@gmail.com>
2024-04-01 19:37:21 -0700

    add google sign in

--- main.js
+++ main.js
@@ -18,3 +18,20 @@ modal.onclick = function(){
     },300);
 }
 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    $("#name").text(profile.getName());
+    $("#email").text(profile.getEmail());
+    $("#image").attr('src', profile.getImageUrl());
+    $(".data").css("display","block");
+    $(".g-signIn2").css("display","none");
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      alert("You have been signed out successfully");
+      $(".g-signIn2").css("display","block");
+      $(".data").css("display","none");
+    });
+  }
